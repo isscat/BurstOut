ec7dd22f71426664af3440c33faf55038bc08cc0
ISS <44267255+isscat@users.noreply.github.com>
2021-01-10 15:11:15 +0530

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,32 @@
+import 'package:DreamsApp/Screens/editing_screen.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() {
+  LicenseRegistry.addLicense(() async* {
+    final license = await rootBundle.loadString('google_fonts/OFL.txt');
+    yield LicenseEntryWithLineBreaks(['google_fonts'], license);
+  });
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setEnabledSystemUIOverlays([SystemUiOverlay.bottom]);
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+        textTheme: TextTheme(
+          headline1: GoogleFonts.peddana(),
+        ),
+      ),
+      home: EditingScreen(),
+    );
+  }
+}
